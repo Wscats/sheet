3d85103399c38c5289ecbc72d84b9af0afc317fa
wscats <enoyao@tencent.com>
2021-09-20 00:28:45 +0800

    support textarea
    support textarea

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,7 +1,16 @@
+// @ts-nocheck
 import prompt from '@system.prompt';
 import Table from './sheet/';
 import Viewport from './sheet/viewport.js';
 export default {
+    props: {
+        areaX: {
+            default: 55,
+        },
+        areaY: {
+            default: 55,
+        },
+    },
     data: {
         sheet: [
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
@@ -47,8 +56,13 @@ export default {
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-
-        ]
+        ],
+        isShowArea: false,
+        areaTop: 0,
+        areaLeft: 0,
+        row: -1,
+        col: -1,
+        content: '',
     },
     touchstart(evt) {
         const cell = this.viewport.cell(evt.touches[0].localX, evt.touches[0].localY);
@@ -58,8 +72,7 @@ export default {
     setTextarea(type, cellRect) {
         const textarea = this.textarea;
         const area = this.area;
-        textarea.style.display = 'none';
-        area.style.display = 'none';
+        this.isShowArea = false;
         switch (type) {
             case 4:
                 const { col, height, row, width, x, y } = cellRect;
@@ -70,24 +83,24 @@ export default {
                     message: `当前值:${showValue} 位置:第${row + 1}行 第${col + 1}列`,
                     duration: 3000,
                 });
-                textarea.style.display = 'flex';
-                area.style.display = 'flex';
-                const frames = [
-                    { transform: { translate: '-120px -0px' }, opacity: 0.1, offset: 0.0 },
-                ];
-                const options = {
-                    width: '100px',
-                };
-                this.$element('area').animate(frames, options);
-                textarea.style.width = width - 8 + 'px';
-                textarea.style.height = height - 4 + 'px';
-                area.style.left = x - 2 + 'px';
-                area.style.top = y - 1 + 'px';
+                this.areaLeft = x - 2;
+                this.areaTop = y - 1;
+                this.isShowArea = true;
                 const value = this.sheet[row][col];
-                textarea.value = typeof value === 'string' || typeof value === 'number' ?
+                this.row = row;
+                this.col = col;
+                this.content = typeof value === 'string' || typeof value === 'number' ?
                     value : value.text;
                 break;
         }
+    },
+    change(e) {
+        this.content = e.value;
+        this.sheet[this.row][this.col] = this.content;
+        this.table.cell((ri, ci) => {
+            return this.sheet[ri][ci];
+        }).render();
+        this.content = '';
     },
     touchend(evt) {
         const range = this.viewport.range(evt.changedTouches[0].localX, evt.changedTouches[0].localY);
